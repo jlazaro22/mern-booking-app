156527692fe6ffd4c6db4065b2d0a70eebd1bb75
Joao Lazaro <joao.lazaro@outlook.com>
2025-05-20 18:55:02 +0100

    backend inital setup

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import 'dotenv/config';
+import express, { json, Request, Response, urlencoded } from 'express';
+
+const host = process.env.HOST || '0.0.0.0';
+const port = Number(process.env.PORT);
+
+const app = express();
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/api/test', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Hello from Express endpoint!' });
+});
+
+app.listen(port, host, () => {
+  console.log(`🚀 Server is running on http://${host}:${port}`);
+});
