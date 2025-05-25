b5ece3a43722894f01d70f5022a5b9c5a6bf91cb
Joao Lazaro <joao.lazaro@outlook.com>
2025-05-25 17:08:50 +0100

    setup test database

--- backend/src/lib/mongoose.ts
+++ backend/src/lib/mongoose.ts
@@ -2,12 +2,23 @@ import mongoose from 'mongoose';
 
 export default function mongooseConnect() {
   const db = process.env.MONGODB_CONNECTION_STRING;
+  const environment = process.env.NODE_ENV;
 
   if (!db) {
     throw new Error('Missing MONGODB_CONNECTION_STRING environment variable');
   }
 
-  mongoose.connect(db, { dbName: 'mern-booking-app-db' }).then(() => {
-    console.log('✅ Connected to MongoDB');
-  });
+  if (environment === 'development') {
+    mongoose.connect(db, { dbName: 'app-db' }).then(() => {
+      console.log('✅ Connected to MongoDB: ', db);
+    });
+  } else if (environment === 'test') {
+    mongoose.connect(db, { dbName: 'app-e2e-test-db' }).then(() => {
+      console.log('✅ Connected to MongoDB: ', db);
+    });
+  } else {
+    mongoose.connect(db, { dbName: 'app-db' }).then(() => {
+      console.log('✅ Connected to MongoDB');
+    });
+  }
 }
